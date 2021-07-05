af1ae23aaafae7264edc65f4b5a305ceb1f5cc94
jamescondon <jecondon66@icloud.com>
2021-07-05 17:39:52 +0100

    Added MAXIMUM LIMIT

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,14 @@
 class Oystercard
 
 attr_reader :bal
+MAXIMUM_BALANCE = 90
 
     def initialize
         @bal = 0
     end
 
     def top_up(amt)
+        fail "Your balance is already: £#{@bal}. This transaction would take over the £#{MAXIMUM_BALANCE} limit." if (@bal+amt) > 90
         @bal += amt
     end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,12 +11,20 @@ describe Oystercard do
 
 
 describe '#top_up' do
+let(:bal) { subject.bal }
+
     it 'Card responds to top_up' do
         expect(subject).to respond_to(:top_up).with(1).argument
     end
     it 'top_up will add to balance' do
         expect{ subject.top_up(1) }.to change{ subject.bal }.by(1)
     end
+    it 'Raises error if attempting to add more than £90 limit' do
+        maximum_balance = Oystercard::MAXIMUM_BALANCE
+        subject.top_up(maximum_balance)
+        expect { subject.top_up(1) }.to raise_error "Your balance is already: £#{bal}. This transaction would take over the £#{maximum_balance} limit."
+    end
+
 end
 
 end
