5f1f8a102f537f42d04c7ab9c99c93741c6c4297
William Young <wadyoung@outlook.com>
2021-07-06 14:46:01 +0100

    Added touch in/out and minimum req

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,16 +1,32 @@
 class Oystercard
-
+  
   attr_reader :balance
+  attr_accessor :in_use
   MAXIMUM_BALANCE = 90
+  MINIMUM = 1
 
   def initialize
     @balance = 0
+    @in_use = false
   end
 
   def top_up(amount)
     fail "Your balance is already: £#{@balance}."\
-    "This transaction would take over the £#{MAXIMUM_BALANCE} limit."\
+     "This transaction would take over the £#{MAXIMUM_BALANCE} limit."\
     if (@balance+amount) > 90
     @balance += amount
+  end
+
+  def deduct(amount)
+    @balance -= amount
+  end
+  
+  def touch_in
+    fail "Require at least £#{MINIMUM} to travel." if @balance < 1
+    @in_use = true
+  end
+
+  def touch_out
+    @in_use = false
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,10 @@
 require 'oystercard'
 
 describe Oystercard do
+  let(:balance) { subject.balance }
+  let(:maximum_balance) { Oystercard::MAXIMUM_BALANCE }
+  let(:minimum) { Oystercard::MINIMUM }
+
   it 'Creates an instance of Oystercard' do
     expect(subject).to be_instance_of(Oystercard)
   end
@@ -10,8 +14,6 @@ describe Oystercard do
   end
 
   describe '#top_up' do
-    let(:balance) { subject.balance }
-    let(:maximum_balance) { Oystercard::MAXIMUM_BALANCE }
 
     it 'Card responds to top_up' do
       expect(subject).to respond_to(:top_up).with(1).argument
@@ -24,7 +26,40 @@ describe Oystercard do
     it 'Raises error if attempting to add more than £90 limit' do
       subject.top_up(maximum_balance)
       expect { subject.top_up(1) }.to raise_error "Your balance is already: £#{balance}."\
-      "This transaction would take over the £#{maximum_balance} limit."\
+       "This transaction would take over the £#{maximum_balance} limit."\
     end
   end
+
+  describe '#deduct' do
+    it 'Will deduct a given amount' do
+      expect{ subject.deduct(1) }.to change{ subject.balance }.by(-1)
+    end
+  end
+
+  describe '#in_use?' do
+    it 'Will return a false boolean if not in use.' do
+    expect(subject.in_use).to eql(false)
+    end
+  end
+
+  describe '#touch_in' do
+    it 'Will cause in use to become true?' do
+      subject.top_up(2)
+      expect(subject.touch_in).to be(true)
+    end
+
+    it 'Will raise an error if touch_in with < £1' do
+      expect{ subject.touch_in }.to raise_error "Require at least £#{minimum} to travel."
+    end
+  end
+
+  describe '#touch_out' do
+    it 'Will cause in use to become false.' do
+      subject.in_use = true
+      expect(subject.touch_out).to be(false)
+    end
+  end
+
+
+
 end
