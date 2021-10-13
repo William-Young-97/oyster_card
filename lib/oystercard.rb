3a348896c0e0b881c89d997115e95bb67b9267c5
William Young <wadyoung@outlook.com>
2021-10-13 16:22:55 +0100

    Added in limit and refactored

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,22 @@
 class Oystercard
   
+  INITIAL_BALANCE = 0
+  MAX_BALANCE = 90
   attr_reader :balance
 
   def initialize
-    @balance = 0
+    @balance = INITIAL_BALANCE
   end
   
   def top_up(amount)
     @balance += amount
+    fail "Cannot exceed £90" if maximum_limit
   end
+
+  private
+
+  def maximum_limit
+    @balance > MAX_BALANCE
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -12,5 +12,8 @@ describe Oystercard do
       subject.top_up(5)
       expect(subject.balance).to eq(5)
     end
+    it 'Raises an error if adding over £90 total' do
+      expect{subject.top_up(91)}.to raise_error("Cannot exceed £90")
+    end
   end
 end
