f0a92bee574046568f3ddcdf539adf381d6036a5
William Young <wadyoung@outlook.com>
2021-10-13 16:09:03 +0100

    Added top up feature

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,6 +3,13 @@ require 'oystercard'
 describe Oystercard do
   describe '#balance' do
     it "Shows current balance" do
+      expect(subject.balance).to eq(0)
+    end
+  end
+
+  describe '#top_up' do
+    it 'Allows you to add to your balance' do
+      subject.top_up(5)
       expect(subject.balance).to eq(5)
     end
   end
